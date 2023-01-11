9c4e8eead6028fc2a9c3e23b00642e11ae2099a7
shimzofremnik <98681891+shimzofremnik@users.noreply.github.com>
2023-01-11 20:27:59 +0100

    Add files via upload

---
+++ dada.py
@@ -0,0 +1,47 @@
+# Task 1 
+
+city = "London"
+print(city)
+
+
+# Task 2 
+
+city = "berlin:"
+
+population = 3645000
+
+print(city.capitalize(), ":" , str(population))
+
+
+# Task 3
+
+city = "london"
+
+population = 9000000
+
+print("City:", city.capitalize().isidentifier())
+
+
+# Task 4
+
+
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital."
+
+
+print("capital:", text.find("capital"))
+
+# Task 5
+
+
+text = "Berlin straddles the banks of the Spree, which flows into the Havel (a tributary of the Elbe) in the western borough of Spandau."
+
+print(text.split())
+
+# Task 6
+
+text = "Berlin is surrounded by the State of Brandenburg and contiguous with Potsdam, Brandenburg's capital.."
+
+print(text.replace("capital", "capital of Germany"))
+
+
+
